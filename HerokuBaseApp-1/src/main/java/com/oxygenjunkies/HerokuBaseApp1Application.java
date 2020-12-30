7e4b86383f9e264bfbdde585c16e6f91e57876fe
Chris <chrisfender0@gmail.com>
2020-12-29 23:24:31 -0800

    init

---
+++ HerokuBaseApp-1/src/main/java/com/oxygenjunkies/HerokuBaseApp1Application.java
@@ -0,0 +1,13 @@
+package com.oxygenjunkies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuBaseApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuBaseApp1Application.class, args);
+	}
+
+}
